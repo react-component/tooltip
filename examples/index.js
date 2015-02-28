c9e05357e2087842643296212d5f68a5a636356c
liuqin.sheng <liuqin.sheng@alipay.com>
2015-02-28 16:52:54 +0800

    init

---
+++ examples/index.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var Tip = require('rc-tip');
+
+console.log(React,Tip);
+React.render(<Tip />,document.getElementById("__react-content"));
--- examples/simple.js
+++
@@ -1,5 +0,0 @@
-// use jsx to render html, do not modify simple.html
-require('tip/assets/index.css');
-var Component = require('tip');
-var React = require('react');
-React.render(<Component/>, document.getElementById('__react-content'));
