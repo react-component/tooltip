8bf36ba29b140cca45a603ae8eb5a58c1eb3b6f9
yiminghe <yiminghe@gmail.com>
2015-04-09 23:23:36 +0800

    update examples at 1428593016488

--- gh-pages.sh
+++
@@ -1,9 +0,0 @@
-npm run less
-npm run example
-cd ../tip-gh-pages
-rm -rf build/
-mkdir build
-cp -r ../tip/build/ build
-git add --all
-git commit -am "update examples"
-git push origin gh-pages:gh-pages
