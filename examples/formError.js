1d624a9d3d57e341306e034d072f2b826931f607
daqiu <stu.cbd@gmail.com>
2015-12-07 17:34:16 +0800

    devDependencies + object-assign && eslint

--- examples/formError.js
+++ examples/formError.js
@@ -1,44 +1,43 @@
-'use strict';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Tooltip from 'rc-tooltip';
 import 'rc-tooltip/assets/bootstrap.less';
 
-var Test = React.createClass({
+const Test = React.createClass({
   getInitialState() {
     return {
-      visible: false
+      visible: false,
     };
   },
-  handleDestroy(){
+  handleDestroy() {
     this.setState({
-      destroy:true
+      destroy: true,
     });
   },
-  handleChange(e){
+  handleChange(e) {
     this.setState({
-      visible:!e.target.value
+      visible: !e.target.value,
     });
   },
   render() {
-    if(this.state.destroy){
+    if (this.state.destroy) {
       return null;
     }
-    return <div >
+    return (<div>
       <div style={{marginTop: 100, marginLeft: 100, marginBottom: 100}}>
         <Tooltip
           visible={this.state.visible}
           animation="zoom"
           trigger={[]}
-          overlayStyle={{zIndex:1000}}
+          overlayStyle={{zIndex: 1000}}
           overlay={<span>required!</span>}>
           <input onChange={this.handleChange}/>
         </Tooltip>
       </div>
       <button onClick={this.handleDestroy}>destroy</button>
-    </div>;
-  }
+    </div>);
+  },
 });
 
-ReactDOM.render(<Test/>, document.getElementById("__react-content"));
+ReactDOM.render(<Test/>, document.getElementById('__react-content'));
--- examples/simple.js
+++ examples/simple.js
@@ -1,4 +1,3 @@
-'use strict';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -9,15 +8,15 @@ import assign from 'object-assign';
 // do not use rc-tooltip/lib/placements
 import placements from 'rc-tooltip/src/placements';
 
-var Test = React.createClass({
+const Test = React.createClass({
   getInitialState() {
-    const placement='right';
+    const placement = 'right';
     const offset = placements[placement].offset;
     return {
-      destroyTooltipOnHide:false,
+      destroyTooltipOnHide: false,
       placement: placement,
       trigger: {
-        hover: 1
+        hover: 1,
       },
       offsetX: offset[0],
       offsetY: offset[1],
@@ -35,38 +34,34 @@ var Test = React.createClass({
 
   onTransitionChange(e) {
     this.setState({
-      transitionName: e.target.checked ? e.target.value : ''
+      transitionName: e.target.checked ? e.target.value : '',
     });
   },
 
   onTriggerChange(e) {
-    var trigger = assign({}, this.state.trigger);
+    const trigger = assign({}, this.state.trigger);
     if (e.target.checked) {
       trigger[e.target.value] = 1;
     } else {
       delete trigger[e.target.value];
     }
     this.setState({
-      trigger: trigger
+      trigger: trigger,
     });
   },
 
   onOffsetXChange(e) {
-    var targetValue = e.target.value;
+    const targetValue = e.target.value;
     this.setState({
       offsetX: targetValue ? targetValue : undefined,
     });
   },
 
   onOffsetYChange(e) {
-    var targetValue = e.target.value;
+    const targetValue = e.target.value;
     this.setState({
       offsetY: targetValue ? targetValue : undefined,
     });
-  },
-
-  preventDefault(e) {
-    e.preventDefault();
   },
 
   onVisibleChange(visible) {
@@ -75,26 +70,30 @@ var Test = React.createClass({
 
   onDestroyCheck() {
     this.setState({
-      destroyTooltipOnHide: !this.state.destroyTooltipOnHide
+      destroyTooltipOnHide: !this.state.destroyTooltipOnHide,
     });
   },
 
+  preventDefault(e) {
+    e.preventDefault();
+  },
+
   render() {
-    var state = this.state;
-    var trigger = state.trigger;
-    return <div >
+    const state = this.state;
+    const trigger = state.trigger;
+    return (<div>
       <div style={{margin: '10px 20px'}}>
         <label>
           placement:
           <select value={this.state.placement} onChange={this.onPlacementChange}>
-            {Object.keys(placements).map(function (p) {
-              return <option key={p} value={p}>{p}</option>
+            {Object.keys(placements).map(function(p) {
+              return <option key={p} value={p}>{p}</option>;
             })}
           </select>
         </label>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <label>
-          <input value='rc-tooltip-zoom' type='checkbox' onChange={this.onTransitionChange}
+          <input value="rc-tooltip-zoom" type="checkbox" onChange={this.onTransitionChange}
                  checked={this.state.transitionName === 'rc-tooltip-zoom'}/>
           transitionName
         </label>
@@ -102,7 +101,7 @@ var Test = React.createClass({
         &nbsp;&nbsp;&nbsp;&nbsp;
 
         <label>
-          <input  type='checkbox' onChange={this.onDestroyCheck}
+          <input type="checkbox" onChange={this.onDestroyCheck}
                  checked={this.state.destroyTooltipOnHide}/>
           destroyTooltipOnHide
         </label>
@@ -112,26 +111,26 @@ var Test = React.createClass({
         trigger:
 
         <label>
-          <input value='hover' checked={trigger.hover} type='checkbox' onChange={this.onTriggerChange}/>
+          <input value="hover" checked={trigger.hover} type="checkbox" onChange={this.onTriggerChange}/>
           hover
         </label>
         <label>
-          <input value='focus' checked={trigger.focus} type='checkbox' onChange={this.onTriggerChange}/>
+          <input value="focus" checked={trigger.focus} type="checkbox" onChange={this.onTriggerChange}/>
           focus
         </label>
         <label>
-          <input value='click' checked={trigger.click} type='checkbox' onChange={this.onTriggerChange}/>
+          <input value="click" checked={trigger.click} type="checkbox" onChange={this.onTriggerChange}/>
           click
         </label>
         <br/>
         <label>
           offsetX:
-          <input type='text' value={state.offsetX} onChange={this.onOffsetXChange} style={{width:50}}/>
+          <input type="text" value={state.offsetX} onChange={this.onOffsetXChange} style={{width: 50}}/>
         </label>
         &nbsp;&nbsp;&nbsp;&nbsp;
         <label>
           offsetY:
-          <input type='text' value={state.offsetY} onChange={this.onOffsetYChange} style={{width:50}}/>
+          <input type="text" value={state.offsetY} onChange={this.onOffsetYChange} style={{width: 50}}/>
         </label>
       </div>
       <div style={{margin: 100}}>
@@ -141,18 +140,18 @@ var Test = React.createClass({
                  destroyTooltipOnHide={this.state.destroyTooltipOnHide}
                  trigger={Object.keys(this.state.trigger)}
                  onVisibleChange={this.onVisibleChange}
-                 overlay={<div style={{height:50,width:50}}>i am a tooltip</div>}
+                 overlay={<div style={{height: 50, width: 50}}>i am a tooltip</div>}
                  align={{
-                  offset:[this.state.offsetX, this.state.offsetY]
+                   offset: [this.state.offsetX, this.state.offsetY],
                  }}
                  transitionName={this.state.transitionName}>
-          <div style={{height:100,width:100,border:'1px solid red'}}>trigger</div>
+          <div style={{height: 100, width: 100, border: '1px solid red'}}>trigger</div>
         </Tooltip>
       </div>
-    </div>;
-  }
+    </div>);
+  },
 });
 
 ReactDOM.render(<div>
   <Test />
-</div>, document.getElementById("__react-content"));
+</div>, document.getElementById('__react-content'));
