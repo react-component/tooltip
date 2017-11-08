46cb8354b0adf863a9bf1c84339a3acd3b87197d
Wei Zhu <yesmeck@gmail.com>
2017-11-08 15:26:42 +0800

    Update dependencies

---
+++ tests/index.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Tooltip from '../src';
+
+const verifyContent = (wrapper, content) => {
+  expect(wrapper.find('.x-content').text()).toBe(content);
+  expect(wrapper.instance().getPopupDomNode()).toBeTruthy();
+  wrapper.find('.target').simulate('click');
+  expect(wrapper.find('.rc-tooltip').hostNodes().hasClass('rc-tooltip-hidden')).toBe(true);
+};
+
+describe('rc-tooltip', () => {
+  describe('shows and hides itself on click', () => {
+    it('using an element overlay', () => {
+      const wrapper = mount(
+        <Tooltip
+          trigger={['click']}
+          placement="left"
+          overlay={<strong className="x-content">Tooltip content</strong>}
+        >
+          <div className="target">Click this</div>
+        </Tooltip>
+      );
+      wrapper.find('.target').simulate('click');
+      verifyContent(wrapper, 'Tooltip content');
+    });
+
+    it('using a function overlay', () => {
+      const wrapper = mount(
+        <Tooltip
+          trigger={['click']}
+          placement="left"
+          overlay={() => (<strong className="x-content">Tooltip content</strong>)}
+        >
+          <div className="target">Click this</div>
+        </Tooltip>
+      );
+      wrapper.find('.target').simulate('click');
+      verifyContent(wrapper, 'Tooltip content');
+    });
+  });
+});
