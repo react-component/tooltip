e56ac1102bece187e2630c5d229a02423a1ef71a
orzyyyy <zy410419243@gmail.com>
2019-10-11 19:25:41 +0800

    chore: update demo

--- examples/onVisibleChange.js
+++ examples/onVisibleChange.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Tooltip from 'rc-tooltip';
-import 'rc-tooltip/assets/bootstrap.less';
+import Tooltip from '../src';
+import '../assets/bootstrap.less';
 
 function preventDefault(e) {
   e.preventDefault();
@@ -11,35 +10,44 @@ class Test extends Component {
   state = {
     visible: false,
   };
-  onVisibleChange = (visible) => {
+
+  onVisibleChange = visible => {
     this.setState({
       visible,
     });
-  }
+  };
+
   onDestroy = () => {
     this.setState({
       destroy: true,
     });
-  }
+  };
+
   render() {
     if (this.state.destroy) {
       return null;
     }
-    return (<div>
-      <div style={{ marginTop: 300, marginLeft: 100, marginBottom: 100 }}>
-        <Tooltip
-          visible={this.state.visible}
-          animation="zoom"
-          onVisibleChange={this.onVisibleChange}
-          trigger="click"
-          overlay={<span>I am a tooltip</span>}
-        >
-          <a href="#" onClick={preventDefault}>trigger</a>
-        </Tooltip>
+    return (
+      <div>
+        <div style={{ marginTop: 300, marginLeft: 100, marginBottom: 100 }}>
+          <Tooltip
+            visible={this.state.visible}
+            animation="zoom"
+            onVisibleChange={this.onVisibleChange}
+            trigger="click"
+            overlay={<span>I am a tooltip</span>}
+          >
+            <a href="#" onClick={preventDefault}>
+              trigger
+            </a>
+          </Tooltip>
+        </div>
+        <button type="button" onClick={this.onDestroy}>
+          destroy
+        </button>
       </div>
-      <button onClick={this.onDestroy}>destroy</button>
-    </div>);
+    );
   }
 }
 
-ReactDOM.render(<Test />, document.getElementById('__react-content'));
+export default Test;
--- examples/placement.js
+++ examples/placement.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Tooltip from 'rc-tooltip';
-import 'rc-tooltip/assets/bootstrap.less';
+import Tooltip from '../src';
+import '../assets/bootstrap.less';
+
 const text = <span>Tooltip Text</span>;
 const styles = {
   display: 'table-cell',
@@ -18,50 +18,74 @@ const rowStyle = {
 };
 
 const Test = () => (
-  <div style={{ display: 'table' }}>
+  <div style={{ display: 'table', padding: 120 }}>
     <div style={rowStyle}>
       <Tooltip placement="left" overlay={text}>
-        <a href="#" style={styles}>Left</a>
+        <a href="#" style={styles}>
+          Left
+        </a>
       </Tooltip>
       <Tooltip placement="top" overlay={text}>
-        <a href="#" style={styles}>Top</a>
+        <a href="#" style={styles}>
+          Top
+        </a>
       </Tooltip>
       <Tooltip placement="bottom" overlay={text}>
-        <a href="#" style={styles}>Bottom</a>
+        <a href="#" style={styles}>
+          Bottom
+        </a>
       </Tooltip>
       <Tooltip placement="right" overlay={text}>
-        <a href="#" style={styles}>Right</a>
+        <a href="#" style={styles}>
+          Right
+        </a>
       </Tooltip>
-    </ div>
+    </div>
     <div style={rowStyle}>
       <Tooltip placement="leftTop" overlay={text}>
-        <a href="#" style={styles}>Left Top</a>
+        <a href="#" style={styles}>
+          Left Top
+        </a>
       </Tooltip>
       <Tooltip placement="leftBottom" overlay={text}>
-        <a href="#" style={styles}>Left Bottom</a>
+        <a href="#" style={styles}>
+          Left Bottom
+        </a>
       </Tooltip>
       <Tooltip placement="rightTop" overlay={text}>
-        <a href="#" style={styles}>Right Top</a>
+        <a href="#" style={styles}>
+          Right Top
+        </a>
       </Tooltip>
       <Tooltip placement="rightBottom" overlay={text}>
-        <a href="#" style={styles}>Right Bottom</a>
+        <a href="#" style={styles}>
+          Right Bottom
+        </a>
       </Tooltip>
     </div>
     <div style={rowStyle}>
       <Tooltip placement="topLeft" overlay={text}>
-        <a href="#" style={styles}>Top Left</a>
+        <a href="#" style={styles}>
+          Top Left
+        </a>
       </Tooltip>
       <Tooltip placement="topRight" overlay={text}>
-        <a href="#" style={styles}>Top Right</a>
+        <a href="#" style={styles}>
+          Top Right
+        </a>
       </Tooltip>
       <Tooltip placement="bottomLeft" overlay={text}>
-        <a href="#" style={styles}>Bottom Left</a>
+        <a href="#" style={styles}>
+          Bottom Left
+        </a>
       </Tooltip>
       <Tooltip placement="bottomRight" overlay={text}>
-        <a href="#" style={styles}>Bottom Right</a>
+        <a href="#" style={styles}>
+          Bottom Right
+        </a>
       </Tooltip>
     </div>
   </div>
 );
 
-ReactDOM.render(<Test />, document.getElementById('__react-content'));
+export default Test;
