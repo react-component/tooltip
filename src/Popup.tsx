f995101a3b4bb83289eccc2e2b4c3fb5c893018a
二货机器人 <smith3816@gmail.com>
2023-02-08 11:13:12 +0800

    chore: clean up

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -5,23 +5,15 @@ export interface ContentProps {
   children: (() => React.ReactNode) | React.ReactNode;
   id?: string;
   overlayInnerStyle?: React.CSSProperties;
-  arrowContent?: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
-  showArrow?: boolean;
 }
 
 export default function Popup(props: ContentProps) {
-  const { showArrow, arrowContent, children, prefixCls, id, overlayInnerStyle, className, style } =
-    props;
+  const { children, prefixCls, id, overlayInnerStyle, className, style } = props;
 
   return (
     <div className={classNames(`${prefixCls}-content`, className)} style={style}>
-      {/* {showArrow !== false && (
-        <div className={`${prefixCls}-arrow`} key="arrow">
-          {arrowContent}
-        </div>
-      )} */}
       <div className={`${prefixCls}-inner`} id={id} role="tooltip" style={overlayInnerStyle}>
         {typeof children === 'function' ? children() : children}
       </div>
