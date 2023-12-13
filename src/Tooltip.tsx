db8fbdfb0f43931f19d8966169cbbc8ea4cfeb95
lhy <lhy_web@163.com>
2023-12-13 20:45:32 +0800

    add forceRender Type

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -16,6 +16,7 @@ export interface TooltipProps
     | 'mouseLeaveDelay'
     | 'mouseEnterDelay'
     | 'prefixCls'
+    | 'forceRender'
   > {
   trigger?: ActionType | ActionType[];
   defaultVisible?: boolean;
