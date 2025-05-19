e351c6bc6107b0f9d02b36a53593d4a4d077323f
lijianan <574980606@qq.com>
2025-05-19 13:01:26 +0800

    fix: fix CI

--- .dumirc.ts
+++ .dumirc.ts
@@ -18,7 +18,6 @@ export default defineConfig({
     name: 'Tooltip',
     logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   },
-  outputPath: '.doc',
   base: isProdSite ? `/${name}/` : '/',
   publicPath: isProdSite ? `/${name}/` : '/',
 });
