d771343f4f73cd2944f50b0f382146d1243c1d96
二货爱吃白萝卜 <smith3816@gmail.com>
2025-09-23 10:40:15 +0800

    chore: popup support style inject (#504)

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -8,16 +8,18 @@ export interface ContentProps {
   id?: string;
   classNames?: TooltipProps['classNames'];
   styles?: TooltipProps['styles'];
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 const Popup: React.FC<ContentProps> = (props) => {
-  const { children, prefixCls, id, classNames, styles } = props;
+  const { children, prefixCls, id, classNames, styles, className, style } = props;
 
   return (
     <div
       id={id}
-      className={cls(`${prefixCls}-body`, classNames?.body)}
-      style={styles?.body}
+      className={cls(`${prefixCls}-body`, classNames?.body, className)}
+      style={{ ...styles?.body, ...style }}
       role="tooltip"
     >
       {typeof children === 'function' ? children() : children}
