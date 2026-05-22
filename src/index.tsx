a7e116d3cff51da1a633203dd696e0de0e8703c0
高艳兵 <yanbing.gao@aliyun.com>
2026-05-22 14:10:03 +0800

    fix: expose tooltip props and placements

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import Popup from './Popup';
 import Tooltip from './Tooltip';
 
-export type { TooltipRef } from './Tooltip';
+export type { TooltipProps, TooltipRef } from './Tooltip';
+export { placements } from './placements';
 export { Popup };
 
 export default Tooltip;
