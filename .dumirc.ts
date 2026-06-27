8b5db461b69d9948801a974fb429af7114319000
afc163 <afc163@gmail.com>
2026-06-27 13:51:22 +0800

    chore: address review comments

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   alias: {
     '@rc-component/tooltip$': path.resolve('src'),
     '@rc-component/tooltip/assets': path.resolve('assets'),
-    '@rc-component/tooltip/assets/*': path.resolve('assets'),
     '@rc-component/tooltip/es': path.resolve('src'),
-    '@rc-component/tooltip/es/*': path.resolve('src'),
   },
   mfsu: false,
   favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
